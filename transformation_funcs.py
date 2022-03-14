69da495dc235ac26b074daff6ab4d9bc4822e5d6
Amir Salimi <asalimi@ualberta.ca>
2022-03-14 14:14:45 -0600

    batch transformation functions working but not batched...

--- transformation_funcs.py
+++ transformation_funcs.py
@@ -1,7 +1,20 @@
 from fastai.vision.augment import RandTransform
 from tsai.all import *
 
-class TSNormalize(RandTransform):
+from scipy.interpolate import CubicSpline
+class Scale(Transform):
+    "Clip  batch of type `TSTensor`"
+    parameters, order = L('min', 'max'), 90
+    def __init__(self, min=-6, max=6, **kwargs):
+        super().__init__(**kwargs)
+        self.min = torch.tensor(min)
+        self.max = torch.tensor(max)
+
+    def encodes(self, o:TSTensor):
+        output = F.interpolate(o,None,0.5)
+        return output
+    def __repr__(self): return f'{self.__class__.__name__}(min={self.min}, max={self.max})'
+class Normalize(Transform):
     # normalize by dividing each sample by its max value
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -10,13 +23,55 @@ class TSNormalize(RandTransform):
         for i in range(len(o)):
             output[i] = output[i]/output[i].max()
         return output
-    
-class Resample(RandTransform):
-    # resampling (probably want downsampling)
-    def __init__(self, size=None, scale_factor=None,**kwargs):
-        self.size = size
-        self.scale_factor = scale_factor
+class MulNoise(Transform):
+    "Applies multiplicative noise on the y-axis for each step of a `TSTensor` batch"
+    order = 90
+    def __init__(self, magnitude=1, ex=None, **kwargs):
+        self.magnitude, self.ex = magnitude, ex
         super().__init__(**kwargs)
     def encodes(self, o: TSTensor):
-        output = F.interpolate(o,self.size,self.scale_factor)
+        if not self.magnitude or self.magnitude <= 0: return o
+        noise = torch.normal(1, self.magnitude * .025, o.shape, dtype=o.dtype, device=o.device)
+        output = o * noise
+        if self.ex is not None: output[...,self.ex,:] = o[...,self.ex,:]
         return output
+class RandomShift(Transform):
+    "Shifts and splits a sequence"
+    order = 90
+    def __init__(self, magnitude=0.02, ex=None, **kwargs):
+        self.magnitude, self.ex = magnitude, ex
+        super().__init__(**kwargs)
+    def encodes(self, o: TSTensor):
+        if not self.magnitude or self.magnitude <= 0: return o
+        pos = int(round(np.random.randint(0, o.shape[-1]) * self.magnitude)) * (random.randint(0, 1)*2-1)
+        output = torch.cat((o[..., pos:], o[..., :pos]), dim=-1)
+        if self.ex is not None: output[...,self.ex,:] = o[...,self.ex,:]
+        return output
+class WindowWarping(Transform):
+    """Applies window slicing to the x-axis of a `TSTensor` batch based on a random linear curve based on
+    https://halshs.archives-ouvertes.fr/halshs-01357973/document"""
+    order = 90
+    def __init__(self, magnitude=0.1, ex=None, **kwargs):
+        self.magnitude, self.ex = magnitude, ex
+        super().__init__(**kwargs)
+    def encodes(self, o: TSTensor):
+        if not self.magnitude or self.magnitude <= 0 or self.magnitude >= 1: return o
+        f = CubicSpline(np.arange(o.shape[-1]), o.cpu(), axis=-1)
+        output = o.new(f(random_cum_linear_generator(o, magnitude=self.magnitude)))
+        if self.ex is not None: output[...,self.ex,:] = o[...,self.ex,:]
+        return output
+class WindowSlicing(Transform):
+    "Randomly extracts an resize a ts slice based on https://halshs.archives-ouvertes.fr/halshs-01357973/document"
+    order = 90
+    def __init__(self, magnitude=0.1, ex=None, mode='linear', **kwargs):
+        "mode:  'nearest' | 'linear' | 'area'"
+        self.magnitude, self.ex, self.mode = magnitude, ex, mode
+        super().__init__(**kwargs)
+    def encodes(self, o: TSTensor):
+        if not self.magnitude or self.magnitude <= 0 or self.magnitude >= 1: return o
+        seq_len = o.shape[-1]
+        win_len = int(round(seq_len * (1 - self.magnitude)))
+        if win_len == seq_len: return o
+        start = np.random.randint(0, seq_len - win_len)
+        return F.interpolate(o[..., start : start + win_len], size=seq_len, mode=self.mode, align_corners=None if self.mode in ['nearest', 'area'] else False)
+
